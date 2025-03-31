b706f2a8e6bf1462be6da9aa3e5129365d6290a7
Y0UG0 <hugo.roux2003@hotmail.fr>
2025-03-31 12:17:48 +0200

    page entreprise + FAQ

--- scripts.js
+++ scripts.js
@@ -60,3 +60,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const faqItems = document.querySelectorAll(".faq-item");
+  
+    faqItems.forEach(item => {
+      const question = item.querySelector(".faq-question");
+  
+      question.addEventListener("click", () => {
+        item.classList.toggle("active");
+      });
+    });
+  });
+  
+  
