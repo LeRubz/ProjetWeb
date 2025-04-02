30f0b594c4e406895ee891bf23f8982715edfdf7
Y0UG0 <hugo.roux2003@hotmail.fr>
2025-04-02 17:42:21 +0200

    rattrapage de la betise monumentale d'alex

--- src/Models/ProfilModel.php
+++ src/Models/ProfilModel.php
@@ -7,14 +7,9 @@ class ProfilModel {
     private $pdo;
 
     public function __construct() {
-
-            $this->pdo = new PDO(
-                'mysql:host=cesi-demo-projetweb.westeurope.cloudapp.azure.com;dbname=projetweb;charset=utf8mb4',  
-                'alexis',  
-                '12345678',  
-                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]  
-            );
-        
+        $this->pdo = new PDO('mysql:host=localhost;dbname=projetweb', 'root', '', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
     }
 
     public function getUserById($id) {
