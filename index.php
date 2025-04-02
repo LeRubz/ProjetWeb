2fa3b9a76aedcda7bbba51be8615d813a175959f
EvanDelbruel <edelbruel@gmail.com>
2025-04-02 17:07:16 +0200

    Layout des liens du footer de caca

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use App\Controllers\EntreprisesController;
 use App\Controllers\ProfilController;
 use App\Controllers\OfferController;
 use App\Controllers\WishlistController;
+use App\Controllers\StaticController; // <-- Nouveau
 
 // Créer une instance de Twig
 $loader = new \Twig\Loader\FilesystemLoader('templates');
@@ -44,9 +45,34 @@ switch ($uri) {
         $controller->WishlistPage();
         break;
 
+    // ✅ Routes statiques :
+    case 'aide':
+        $controller = new StaticController($twig);
+        $controller->aide();
+        break;
+
+    case 'cgu':
+        $controller = new StaticController($twig);
+        $controller->cgu();
+        break;
+
+    case 'cookies':
+        $controller = new StaticController($twig);
+        $controller->cookies();
+        break;
+
+    case 'apropos':
+        $controller = new StaticController($twig);
+        $controller->apropos();
+        break;
+
+    case 'mentions':
+        $controller = new StaticController($twig);
+        $controller->mentions();
+        break;
+
     default:
         http_response_code(404);
         echo "Page '$uri' non trouvée.";
         break;
 }
-
